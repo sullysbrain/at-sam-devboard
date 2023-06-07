8ddf548900af4195b31fb1c7b8e34d3239c71d76
Scott Sullivan <scott@sullysbrain.com>
2023-06-07 18:51:53 -0400

    compiled C to board. must use Mac "fix" with bootloader reset

--- src/main.c
+++ src/main.c
@@ -1,31 +1,27 @@
-#include "sam.h"
+#include <sam.h>
 
-#define LED_PIN PIN_PA17
+#define LED_PIN 17
 
-void delay(uint32_t delay);
+void delay_ms(uint32_t ms);
 
-int main(void) {
-    /* Initialize the SAM system */
-    SystemInit();
-  
-    /* Switch the multiplexer to the GPIO function (N is the port number) */
-    PORT->Group[0].PINCFG[LED_PIN].bit.PMUXEN = 0;
-  
-    /* Set the direction of the LED pin to output */
-    PORT->Group[0].DIRSET.reg = (1 << LED_PIN);
-  
-    /* Start blinking loop */
+int main(void)
+{
+    // Set the pin as output
+    REG_PORT_DIR0 |= (1 << LED_PIN);
+
     while (1) {
-        /* Toggle the LED state */
-        PORT->Group[0].OUTTGL.reg = (1 << LED_PIN);
-      
-        /* Delay to make the LED blinking visible */
-        delay(1000000);
+        // Toggle the LED
+        REG_PORT_OUTTGL0 = (1 << LED_PIN);
+
+        // Delay for a while
+        delay_ms(350);
     }
+
+    return 0;
 }
 
-void delay(uint32_t delay) {
-    while(delay > 0) {
-        delay--;
+void delay_ms(uint32_t ms) {
+    for (uint32_t delay = ms * 4000; delay > 0; delay--) {
+        __asm__ volatile ("nop");
     }
 }
