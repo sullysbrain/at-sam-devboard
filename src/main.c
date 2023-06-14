582569c6dd1f44bf24e475d5e6c0ad5aaaacb035
Scott Sullivan <scott@sullysbrain.com>
2023-06-14 00:39:50 -0400

    Began change to have led pulse using sin()

--- src/main.c
+++ src/main.c
@@ -1,27 +1,46 @@
 #include <sam.h>
+#include <math.h>
 
 #define LED_PIN 17
+#define MAX_BRIGHTNESS 5
+#define STEP_SIZE 5
+#define PI 3.14159265
 
 void delay_ms(uint32_t ms);
 
 int main(void)
 {
-    // Set the pin as output
     REG_PORT_DIR0 |= (1 << LED_PIN);
 
-    while (1) {
-        // Toggle the LED
-        REG_PORT_OUTTGL0 = (1 << LED_PIN);
+    uint8_t brightness = 0;
+    int8_t step = STEP_SIZE;
 
-        // Delay for a while
-        delay_ms(350);
+    while (1) {
+        for (brightness = 0; brightness <= MAX_BRIGHTNESS; brightness += STEP_SIZE) {
+            // LED on
+            REG_PORT_OUTCLR0 = (1 << LED_PIN);
+            delay_ms(brightness);
+
+            // LED off
+            REG_PORT_OUTSET0 = (1 << LED_PIN);
+            delay_ms((int)(sin((1 - brightness) * PI) * MAX_BRIGHTNESS));
+        }
+        for (brightness = MAX_BRIGHTNESS; brightness >= 0; brightness -= STEP_SIZE) {
+            // LED on
+            REG_PORT_OUTCLR0 = (1 << LED_PIN);
+            delay_ms((int)(sin(brightness * PI) * MAX_BRIGHTNESS));
+
+            // LED off
+            REG_PORT_OUTSET0 = (1 << LED_PIN);
+            delay_ms(MAX_BRIGHTNESS - brightness);
+        }
     }
 
     return 0;
 }
 
 void delay_ms(uint32_t ms) {
-    for (uint32_t delay = ms * 4000; delay > 0; delay--) {
+    for (uint32_t delay = ms * 4; delay > 0; delay--) {
         __asm__ volatile ("nop");
     }
 }
