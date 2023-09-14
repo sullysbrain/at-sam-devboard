36a6912aaf79d3472199fb74ca52b37b08cca7c2
Scott Sullivan <scott@sullysbrain.com>
2023-09-13 20:44:03 -0400

    Pruned space.

--- src/main.c
+++ src/main.c
@@ -2,23 +2,19 @@
 #include <math.h>
 #include <stdio.h>
 
-#define LED_PIN 17  // PA17
-#define MAX_BRIGHTNESS 5
-#define STEP_SIZE 5
-#define PI 3.14159265
+#define LED_PIN 17  // blue led
+#define WAVE_PERIOD 32768 // 1Hz
 
-// external variables
-extern int my_public_int;
+void SystemInit() {
 
-
-// Internal Math functions
-int double_int(int input);
-void delay_ms(uint32_t ms);
+}
 
 
 int main(void){
-    // Enable the TCC1 clock in the Power Manager
-    PM->APBCMASK.reg |= PM_APBCMASK_TCC1;
+    //SystemInit();
+
+    // Enable the TCC0 clock in the Power Manager
+    PM->APBCMASK.reg |= PM_APBCMASK_TCC2;
 
     // Enable the OSC32K oscillator
     SYSCTRL->OSC32K.reg = SYSCTRL_OSC32K_STARTUP(6) | // Startup time
@@ -27,7 +23,7 @@ int main(void){
     while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_OSC32KRDY)); // Wait for OSC32K to be ready
 
     // Set up GCLK7 to use OSC32K
-    GCLK->GENDIV.reg = GCLK_GENDIV_ID(7) | GCLK_GENDIV_DIV(32768); // Divide by 32768
+    GCLK->GENDIV.reg = GCLK_GENDIV_ID(7); // Divide by 1
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY); // Wait for synchronization
 
     GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(7) | // Select GCLK7
@@ -35,26 +31,32 @@ int main(void){
                         GCLK_GENCTRL_GENEN; // Enable GCLK7
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY); // Wait for synchronization
 
-    // Connect GCLK7 to TCC1
-    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_TCC0_TCC1 |  // Select TCC1/TCC0
+    // Connect GCLK7 to TCC0
+    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_TCC2_TC3 |  // Select TCC1/TCC0
                         GCLK_CLKCTRL_GEN_GCLK7 |    // Select GCLK7
                         GCLK_CLKCTRL_CLKEN;         // Enable this clock
-    while(GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY); // Wait for synchronization
 
-    // Configure PA17 with peripheral function F
+    // Configure PA17 with peripheral function E
     PORT->Group[0].PINCFG[17].bit.PMUXEN = 1; // Enable the peripheral multiplexer on PA17
-    PORT->Group[0].PMUX[17 >> 1].bit.PMUXO = PORT_PMUX_PMUXO_F_Val; // Set the multiplexer to peripheral function F
+    PORT->Group[0].PMUX[17 >> 1].bit.PMUXO = PORT_PMUX_PMUXO_E_Val; // Set the multiplexer
+    // // set LED to output and turn on
+    // PORT->Group[0].DIRSET.reg |= (1 << LED_PIN);
+    // PORT->Group[0].OUTSET.reg |= (1 << LED_PIN);
 
-    // Configure TCC1
-    TCC1->CTRLA.bit.PRESCALER = TCC_CTRLA_PRESCALER_DIV1_Val; // No additional division
-    TCC1->PER.reg = 1;  // With the 1Hz GCLK7, this gives a frequency of 1Hz
+    // Configure TCC0
+    TCC0->CTRLA.reg = TCC_CTRLA_SWRST; // Reset TCC0
+    while (TCC0->SYNCBUSY.reg & TCC_SYNCBUSY_SWRST); // Wait for the reset to complete
 
-    TCC1->WAVE.bit.WAVEGEN = TCC_WAVE_WAVEGEN_NPWM_Val;  // Set up for normal (single-slope) PWM mode
-    TCC1->CC[1].reg = 1 / 2; // 50% duty cycle
-    TCC1->CTRLA.bit.ENABLE = 1;  // Enable TCC1
-    while(TCC1->SYNCBUSY.bit.ENABLE); // Wait for synchronization
+    TCC2->CTRLA.bit.PRESCALER = TCC_CTRLA_PRESCALER_DIV1_Val; // No additional division
+    TCC2->PER.reg = 32768; // With the 1Hz GCLK7, this gives a frequency of 1Hz
+    TCC2->WAVE.bit.WAVEGEN = TCC_WAVE_WAVEGEN_NPWM_Val; // Set up for normal (single-slope) PWM mode
+    TCC2->CC[1].reg = 32768 / 4; // 50% duty cycle
+    TCC2->CTRLA.bit.ENABLE = 1; // Enable TCC0
+    while(TCC2->SYNCBUSY.bit.ENABLE); // Wait for synchronization
 
-    while (1){
+    while(1) {
+        asm volatile ("nop");
     }
 
+    return 0;
 }
